1d3fc11d6b3f443cc88b58c90a8c7e3ce5fe51bf
MartinOrl <53393723+MartinOrl@users.noreply.github.com>
2020-11-04 23:29:34 +0100

    try 4

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/main.html'))
 })
 
-app.listen(3030, (req,res) => {
-    console.log("Server is running on port", process.env.PORT)
+app.listen(process.env.PORT, (req,res) => {
+    console.log("Server is running on port")
 })
