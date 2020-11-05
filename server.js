f10354afa0c5549fe5a33eeb9916f2b027314dba
MartinOrl <53393723+MartinOrl@users.noreply.github.com>
2020-11-05 12:53:27 +0100

    test5

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const favicon = require('serve-favicon')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(favicon(path.join(__dirname,'public','favicon.ico')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/main.html'))
 })
 
-app.listen(process.env.PORT, (req,res) => {
+app.listen(3030, (req,res) => {
     console.log("Server is running on port")
 })
